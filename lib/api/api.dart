7b96a9dbb9702350b38da736843b887a49f7405a
Vishal Chandna <vishalchandna@Vishals-iMac.local>
2020-09-11 21:29:19 +0530

    Base getAccounts function added.

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,8 +1,18 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_hive/constants/constants.dart';
 
 class HiveApi {
+  Random random = new Random(99);
   Dio dio = new Dio();
+
+  Map<String, dynamic> baseApiPayload = {
+    'id': '1',
+    'jsonrpc': "2.0",
+    'method': "call",
+    'params': [],
+  };
 
   /// The response will be passed in [callback] eg. callback({dynmaic value}){}
   Future<Map<String, dynamic>> postApi({dynamic payload, Function callback}) async {
@@ -21,5 +31,21 @@ class HiveApi {
   String setBaseOrigin(String value) {
     BASE_HIVE_API_URL = value;
     return BASE_HIVE_API_URL;
+  }
+
+  Map<String, dynamic> getPayload({dynamic params, Map<String, dynamic> payload}) {
+    if (payload == null) payload = {};
+    return {...baseApiPayload, ...payload, 'params': params};
+  }
+
+  Future<dynamic> getAccounts(List<String> value) async {
+    Map<String, dynamic> payload = getPayload(params: [
+      "database_api",
+      "get_accounts",
+      [
+        [...value]
+      ],
+    ]);
+    return await postApi(payload: payload);
   }
 }
