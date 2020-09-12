aad6c50fc0501c0ea43f7899c0c0e0db465b6642
Vishal Chandna <vishalchandna@Vishals-iMac.local>
2020-09-12 21:40:29 +0530

    Function access modifiers added.

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -7,7 +7,7 @@ class HiveApi {
   Random random = new Random(99);
   Dio dio = new Dio();
 
-  Map<String, dynamic> baseApiPayload = {
+  Map<String, dynamic> _baseApiPayload = {
     'id': '1',
     'jsonrpc': "2.0",
     'method': "call",
@@ -15,7 +15,7 @@ class HiveApi {
   };
 
   /// The response will be passed in [callback] eg. callback({dynmaic value}){}
-  Future<Map<String, dynamic>> postApi({dynamic payload, Function callback}) async {
+  Future<Map<String, dynamic>> _postApi({dynamic payload, Function callback}) async {
     Response response = await dio.post(BASE_HIVE_API_URL, data: payload);
     if (response.statusCode == 200 && response.data != null && response.data["result"] != null) {
       if (callback != null) {
@@ -27,25 +27,26 @@ class HiveApi {
     }
   }
 
+  Map<String, dynamic> _getPayload({dynamic params, Map<String, dynamic> payload}) {
+    if (payload == null) payload = {};
+    return {..._baseApiPayload, ...payload, 'params': params};
+  }
+
   /// This will change the default BASE_HIVE_API_URL to the passed argument eg. setBaseOrigin("https://api.hive.blog")
   String setBaseOrigin(String value) {
     BASE_HIVE_API_URL = value;
     return BASE_HIVE_API_URL;
   }
 
-  Map<String, dynamic> getPayload({dynamic params, Map<String, dynamic> payload}) {
-    if (payload == null) payload = {};
-    return {...baseApiPayload, ...payload, 'params': params};
-  }
-
+  /// Fetch accounts with [value] as a List of usernames on Hive blockchain. e.g getAccounts(["funnyman", "crazyman"])
   Future<dynamic> getAccounts(List<String> value) async {
-    Map<String, dynamic> payload = getPayload(params: [
+    Map<String, dynamic> payload = _getPayload(params: [
       "database_api",
       "get_accounts",
       [
         [...value]
       ],
     ]);
-    return await postApi(payload: payload);
+    return await _postApi(payload: payload);
   }
 }
