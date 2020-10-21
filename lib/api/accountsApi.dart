2736a4d40700299ce1751a5c897ee7ca85446e9d
Vishal Chandna <vishalchandna@Vishals-iMac.local>
2020-10-21 20:04:28 +0530

    lookupAccounts api function added.

--- lib/api/accountsApi.dart
+++ lib/api/accountsApi.dart
@@ -28,4 +28,21 @@ extension AccountsApi on HiveApi {
     ]);
     return await _postApi(payload: payload, callback: callback);
   }
+
+  /// Fetch accounts with [lowerBoundName] as the lowerbound name e.g lookupAccounts("blo")
+  ///
+  /// [limit], Number of accounts you want in the response, int, default value is `20`
+  ///
+  /// The response will be passed in [callback] eg. callback({dynmaic value}){}
+  Future<Map<String, dynamic>> lookupAccounts({List<String> lowerBoundName, int limit = 20, Function callback}) async {
+    Map<String, dynamic> payload = _getPayload(params: [
+      "database_api",
+      "lookup_accounts",
+      [
+        lowerBoundName,
+        limit,
+      ],
+    ]);
+    return await _postApi(payload: payload, callback: callback);
+  }
 }
